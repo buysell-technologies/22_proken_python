8f929e3253765272a743bfa41cfca73550d83d25
Tamarihiroto <h.tamari@buysell-technologies.com>
2022-06-07 16:34:03 +0900

    feat: add init page

---
+++ app.py
@@ -0,0 +1,65 @@
+import os
+# Use the package we installed
+from slack_bolt import App
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# Add functionality here
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "input",
+            "element": {
+              "type": "plain_text_input",
+              "multiline": True,
+              "action_id": "plain_text_input-action"
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "疑問を投稿しよう",
+              "emoji": True
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "投稿する",
+                  "emoji": True
+                },
+                "style": "primary",
+                "value": "send_question",
+                "action_id": "send-question"
+              }
+            ]
+          }
+        ]
+      }
+    )
+  
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
